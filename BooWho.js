a9e5156f8fcf48da0e7f2f047e8ceb1bc7879d97
Raman Heera <rheera2@uwo.ca>
2020-06-24 15:47:31 -0400

    Create BooWho.js
    - check if that value passed is a boolean primitive (either true or false)

---
+++ BooWho.js
@@ -0,0 +1,12 @@
+/* Check if a value is classified as a boolean primitive. Return true or false. */
+
+function booWho(bool) {
+    if (bool === true || bool === false){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+booWho(null);
